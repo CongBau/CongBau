5abbfe3758fe544f9566b9269db030a745897069
CongBau <bauchen307@gmail.com>
2023-11-08 15:41:09 +0700

    PHP

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+   <body> 
+    <?php 
+    echo "MY FIRST PHP FILE";
+     ?>
+     <h3>Hello World!<h3>
+     <select>
+        <option>1</option>
+        <option>2</option>
+      </select>
+   </body>
+</html>
