646e03c37ab878adb39660f757c32be08163c755
CongBau <bauchen307@gmail.com>
2023-12-08 23:35:02 +0700

    thuchanh

--- php/funcotion.php
+++ php/funcotion.php
@@ -1,13 +1,21 @@
-<?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$db = 'mydatab';
-$conn = new mysqli($servername, $username, $password, $db);
-if ($conn->connect_error)
-{
-  die('Connection failed:' . $conn->connect_error);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ThanCongBAu_2121050398</title>
+</head>
+<body>
+     <p> nhập điểm toán lý hóa của bạn </P>
+     <form action="funcontion.php" method="POST">
+          <p> nhập điểm  toán </p> 
+          <input type="text" name="toan"> <br>
+          <p>nhập điểm lý </p>
+          <input type="text" name="ly"> <br>
+          <p>nhập điểm hóa </p>
+          <input type="text" name="hoa"> <br>
+          <input type="submit">
+     </form>
 
-}
-echo "Connected successfully <br>";
-?>
+</body>
+</html>
